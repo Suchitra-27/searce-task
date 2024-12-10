f109829b20f54e5a0e382efd02b4e3904a98912c
Suchitra-27 <139349673+Suchitra-27@users.noreply.github.com>
2024-12-10 16:37:21 +0530

    position.module.ts

---
+++ frontend/Angular_18_happier_work/happy_work/src/app/position/position.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PositionListComponent } from './position-list/position-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatOption, MatSelect } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconButton } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CreatePositionComponent } from './create-position/create-position.component';
+
+
+const routes: Routes = [
+
+  { path: 'list', component: PositionListComponent },
+];
+
+@NgModule({
+  declarations: [PositionListComponent, CreatePositionComponent],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormField,
+    MatSelect,
+    MatOption,
+    MatLabel,
+    MatIconModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconButton,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class PositionModule { }
